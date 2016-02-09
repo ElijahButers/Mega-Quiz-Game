adf1b1c642139b75207478f1577019db2e3dcc31
ElijahButers <Elijahbuters@gmail.com>
2016-02-09 15:39:56 +0200

    Initial Commit

---
+++ Mega.Quiz.Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mega.Quiz.Game
+//
+//  Created by User on 2/9/16.
+//  Copyright © 2016 Elijah Buters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
