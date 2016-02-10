67e3c851da701352d3de3f8052ab6b4c8a5ebc5d
ElijahButers <Elijahbuters@gmail.com>
2016-02-10 17:50:21 +0200

    Added 5 button outlets, 2 oulet labels.

--- Mega.Quiz.Game/ViewController.swift
+++ Mega.Quiz.Game/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonA: UIButton!
+    @IBOutlet weak var buttonB: UIButton!
+    @IBOutlet weak var buttonC: UIButton!
+    @IBOutlet weak var buttonD: UIButton!
+    @IBOutlet weak var buttonNext: UIButton!
+    
+    @IBOutlet weak var labelQuestion: UILabel!
+    @IBOutlet weak var labelFeedback: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
