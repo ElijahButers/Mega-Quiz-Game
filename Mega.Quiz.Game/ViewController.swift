657a8de3c7eff11b94afc4a93292ebf284d228e3
ElijahButers <Elijahbuters@gmail.com>
2016-10-12 20:02:36 +0300

    Renamed a score label, reconnected the score label, feedback label.

--- Mega.Quiz.Game/ViewController.swift
+++ Mega.Quiz.Game/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var labelQuestion: UILabel!
     @IBOutlet weak var labelFeedback: UILabel!
-    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var labelScore: UILabel!
     
     var allEntries: NSArray!
     var currentCorrectAnswerIndex: Int = 0
